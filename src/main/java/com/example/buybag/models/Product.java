8e31a4b346ec047c000677b1be2d30f755aba6c8
saikalbatyrbekova <saikal.batyrbekova@alatoo.edu.kg>
2024-03-24 16:14:45 +0600

    Initial commit

---
+++ src/main/java/com/example/buybag/models/Product.java
@@ -0,0 +1,57 @@
+package com.example.buybag.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "products")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @jakarta.persistence.Id
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "description", columnDefinition = "text")
+    private String description;
+    @Column(name = "price")
+    private int price;
+    @Column(name = "city")
+    private String city;
+    @Column(name = "author")
+    private String author;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "product")
+    private List<Image> images = new ArrayList<>();
+    private Long previewImageId;
+    private LocalDateTime dateOfCreated;
+
+    @PrePersist
+    private void init (){
+        dateOfCreated = LocalDateTime.now();
+    }
+
+    public void addImageToProduct(Image image){
+        image.setProduct(this);
+        images.add(image);
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
